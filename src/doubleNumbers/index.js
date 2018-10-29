6d1914ad008b7c2bc619eb15661268a1491e6d79
Zlampa <nadja.seniv@visahq.ua>
2018-10-29 19:13:09 +0200

    implemented_solutions

--- src/doubleNumbers/index.js
+++ src/doubleNumbers/index.js
@@ -1,4 +1,12 @@
 /* eslint-disable-next-line */
 export const doubleNum = num => {
-  // your logic here...
+  const str = String(num);
+  const halfLength = str.length/2;
+  const firstPart = str.substr(0, halfLength);
+  const secondPart = str.substr(halfLength);
+  if (halfLength >= 1 && firstPart === secondPart) {
+    return num;
+  } else {
+    return num * 2;
+  }
 };
--- src/sortByWeight/index.js
+++ src/sortByWeight/index.js
@@ -1,4 +1,7 @@
 /* eslint-disable-next-line */
 export const sortByWeight = arr => {
-  // your logic here...
+  const sumElement = val => {
+    return val.split('').reduce((a, b) => Number(a) + Number(b), 0);
+  }
+  return arr.sort((a, b) => sumElement(a) - sumElement(b));
 };
